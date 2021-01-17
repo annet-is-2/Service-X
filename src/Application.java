da6a6b52074bc86640920f14c5d2c25e8a1d41be
annet-is-2 <Anna_s.kovaleva@yahoo.com>
2021-01-17 11:52:23 +0200

    3rd commit

--- src/Application.java
+++ src/Application.java
@@ -3,5 +3,6 @@ public class Application {
         System.out.println("Hello, World!");
         System.out.println("Alexey has started to learn Java.");
         System.out.println("hello");
+        System.out.println("3rd commit");
     }
 }
