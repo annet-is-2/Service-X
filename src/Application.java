c978de3193eb76137307f7d65990afa3f9e38092
annet-is-2 <Anna_s.kovaleva@yahoo.com>
2021-01-17 11:40:47 +0200

    2nd commit

--- src/Application.java
+++ src/Application.java
@@ -2,5 +2,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Alexey has started to learn Java.");
+        System.out.println("hello");
     }
 }
